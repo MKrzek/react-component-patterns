b041d28b8bae4d5eff361c315eba9880c4a26a8d
MKrzek <mkrzek@googlemail.com>
2020-06-17 14:06:25 +0100

    refactor: used context with compound components

--- src/components/Toggle-compoundUsage.js
+++ src/components/Toggle-compoundUsage.js
@@ -1,5 +1,7 @@
 import React, { useState, useContext } from 'react';
-import { Switch } from "./Switch"
+import { Switch } from "./Switch";
+const ToggleContext = React.createContext()
+
 class ToggleComponent extends React.Component {
 
     state = { on: false }
@@ -7,17 +9,31 @@ class ToggleComponent extends React.Component {
     toggle = () => this.setState(({ on }) => ({on: !on}), () => [
         this.props.onToggle(this.state.on)
     ])
-    static On = ({ on, children }) => { console.log('chill', children); return (on ? children : null) }
-    static Off = ({ on, children }) => (on ? null : children)
-    static Button = ({ on, toggle, ...props }) => (
-        <Switch on = { on } onClick = { toggle }{...props }/>
+    static On = ({ on, children }) => (
+        <ToggleContext.Consumer>
+        {contextValue => (
+                contextValue.on ? children : null)}
+        </ToggleContext.Consumer>)
+
+    static Off = ({ on, children }) => (
+        <ToggleContext.Consumer>
+            {contextValue => contextValue.on ? null : children}
+        </ToggleContext.Consumer>)
+
+    static Button = (props) => (
+        <ToggleContext.Consumer>
+            {contextValue=>(<Switch on={contextValue.on} onClick={contextValue.toggle}{...props} />)}
+        </ToggleContext.Consumer>
     )
+
     render() {
         const { on } = this.state
-        console.log('children', this.props.children)
-        return (
-            React.Children.map(this.props.children, childElement => React.cloneElement(childElement, { on, toggle: this.toggle }))
-    );
+        return <ToggleContext.Provider value={{ on, toggle: this.toggle }}>
+            {this.props.children}
+        </ToggleContext.Provider>
+    //     return (
+    //         React.Children.map(this.props.children, childElement => React.cloneElement(childElement, { on, toggle: this.toggle }))
+    // );
     }
 };
 
